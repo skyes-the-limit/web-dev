db281051781e3d66c704affe709ecea9bdac2366
skyes-the-limit <riversong477@gmail.com>
2022-02-14 20:20:37 -0500

    TODO List

---
+++ public/labs/a5/todos/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+
+import TodoList from "./TodoList.js";
+
+$('#wd-todo').append(`
+    <div class="container">
+        <h1>Todo example</h1>
+        ${TodoList()}
+    </div>
+`);
