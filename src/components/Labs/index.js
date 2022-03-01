23732f79f210dc4d893a8ae8f66e3248facc2731
skyes-the-limit <riversong477@gmail.com>
2022-02-28 19:47:39 -0500

    Styles, Classes, Conditionals

---
+++ src/components/Labs/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Styles from "./Styles";
+import Classes from "./Classes";
+import ConditionalOutput from "./ConditionalOutput";
+
+const Labs = () => {
+  return (
+    <div>
+      <h1>Labs</h1>
+      <Styles />
+      <Classes />
+      <ConditionalOutput />
+    </div>
+  )
+};
+
+
+export default Labs;
