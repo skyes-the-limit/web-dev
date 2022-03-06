6678b5cf2b4e52db48394d5ab5d8d8a0c429fb82
skyes-the-limit <riversong477@gmail.com>
2022-03-06 18:30:29 -0500

    Cleanup page layout

--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -6,10 +6,22 @@ import "./index.css"
 
 const Tuiter = () => {
   return (
-    <>
-      <NavigationSidebar active="home" />
-      <WhoToFollowList />
-    </>
+    <div class="row">
+      <div
+        id="tuiter-nav"
+        class="col-2 col-sm-3 col-md-2 col-lg-2 col-xl-2 col-xxl-2"
+      >
+        <NavigationSidebar active="home" />
+      </div>
+      <div
+        id="wd-home-body"
+        class="col-10 col-sm-9 col-md-10 col-lg-6 col-xl-6 col-xxl-7"
+      >
+      </div>
+      <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 col-xxl-3">
+        <WhoToFollowList />
+      </div>
+    </div>
   )
 };
 
