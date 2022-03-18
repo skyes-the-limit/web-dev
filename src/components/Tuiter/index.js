0e10e80d148c67c708c9149103d2b3aef83ea3cd
skyes-the-limit <riversong477@gmail.com>
2022-03-18 18:17:49 -0400

    WhoToFollow

--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -1,35 +1,41 @@
 import { Outlet } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import whoReducer from "./reducers/who-reducer";
 import NavigationSidebar from "./NavigationSidebar";
-// import WhoToFollowList from "./WhoToFollowList";
+import WhoToFollowList from "./WhoToFollowList";
 
 import "./tuiter.css";
 
+const store = createStore(whoReducer);
+
 const Tuiter = () => {
   return (
-    <div className="row mt-2">
-      <div
-        id="tuiter-nav"
-        className="col-2 col-sm-3 col-md-2 col-lg-2 col-xl-2 col-xxl-2"
-      >
-        <NavigationSidebar />
-      </div>
+    <Provider store={store}>
+      <div className="row mt-2">
+        <div
+          id="tuiter-nav"
+          className="col-2 col-sm-3 col-md-2 col-lg-2 col-xl-2 col-xxl-2"
+        >
+          <NavigationSidebar />
+        </div>
 
-      <div
-        id="tuiter-body"
-        className="col-10 col-sm-9 col-md-10 col-lg-6 col-xl-6 col-xxl-7"
-      >
-        <Outlet />
-      </div>
+        <div
+          id="tuiter-body"
+          className="col-10 col-sm-9 col-md-10 col-lg-6 col-xl-6 col-xxl-7"
+        >
+          <Outlet />
+        </div>
 
-      <div
-        id="tuiter-who-to-follow"
-        className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 col-xxl-3"
-      >
-        <h1>Who to Follow</h1>
-        {/* <WhoToFollowList /> */}
+        <div
+          id="tuiter-who-to-follow"
+          className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 col-xxl-3"
+        >
+          <WhoToFollowList />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
