bf82fc600e1f95854776e40dd01a3e9e810b4efe
skyes-the-limit <riversong477@gmail.com>
2022-03-19 17:30:36 -0400

    Update ProfileScreen header area

--- src/components/Tuiter/ProfileScreen/index.js
+++ src/components/Tuiter/ProfileScreen/index.js
@@ -9,29 +9,55 @@ import './index.css'
 const ProfileScreen = () => {
   let [editing, setEditing] = useState(false)
   const profile = useSelector(state => state.profile);
-  console.log(profile)
+  const { firstName, lastName, profilePicture, bannerPicture, tweetCount } = profile
 
   return (
     <div
       id="wd-profile"
+      className='d-flex flex-column'
     >
-      {editing ?
-        <i className="fas fa-times"></i> :
-        <i className="fas fa-arrow-left"></i>
-      }
-      <h2>{editing ? "Edit profile" : "USERNAME"}</h2>
-      {editing &&
-        <button onClick={() => setEditing(false)}>
-          Save
-        </button>
-      }
-      <p>Banner image</p>
-      <p>Profile image potentially with overlay</p>
-      {!editing &&
-        <button onClick={() => setEditing(true)}>
-          Edit profile
-        </button>
-      }
+      <div className='d-flex justify-content-between mb-2'>
+        <div className='d-flex align-items-center'>
+          <div className='me-4'>
+            {editing ?
+              <i className="fas fa-times"></i> :
+              <i className="fas fa-arrow-left 2x"></i>
+            }
+          </div>
+
+          {editing ?
+            <h5 className='profile-header'>Edit profile</h5> :
+            <span>
+              <h5 className='profile-header'>{firstName} {lastName}</h5>
+              <p className='profile-tweetCount'>{tweetCount} Tweets</p>
+            </span>
+          }
+        </div>
+
+        {editing &&
+          <button
+            onClick={() => setEditing(false)}
+            className='btn btn-light'
+          >
+            Save
+          </button>
+        }
+      </div>
+
+      <img className='profile-bannerPicture' src={bannerPicture} alt={`${firstName}'s banner`} />
+
+      <div className='d-flex justify-content-between'>
+        <img className='profile-profilePicture' src={profilePicture} alt={`${firstName}'s profile`} />
+        {!editing &&
+          <button
+            onClick={() => setEditing(true)}
+            className='btn btn-secondary'
+          >
+            Edit profile
+          </button>
+        }
+      </div>
+
       {editing ? <EditProfile /> : <ViewProfile />}
     </div>
   )
